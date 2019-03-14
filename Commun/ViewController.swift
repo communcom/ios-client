bc4df6bdcaaa2f0e5032dcf9dcecc2c3be1f7404
Prigozhenkov <maxprig@gmail.com>
2019-03-14 16:49:19 +0300

    Initial Commit

---
+++ Commun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Commun
+//
+//  Created by Maxim Prigozhenkov on 14/03/2019.
+//  Copyright © 2019 Maxim Prigozhenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
