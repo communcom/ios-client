08774a9e06155ef1771c2e107ba5b8363d8bdb52
Chung Tran <bigearsenal@gmail.com>
2019-04-17 17:57:20 +0300

    add shadow to views

---
+++ Commun/Extensions/UIView+Shadow.swift
@@ -0,0 +1,30 @@
+//
+//  UIView+Shadow.swift
+//  Commun
+//
+//  Created by Chung Tran on 17/04/2019.
+//  Copyright © 2019 Maxim Prigozhenkov. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    @IBInspectable var defaultShadow: Bool {
+        get {
+            return layer.shadowPath != nil ? true: false
+        }
+        set {
+            if (newValue == false) {
+                layer.masksToBounds = true
+                layer.shadowOffset = CGSize(width: 0, height: 0)
+                layer.shadowOpacity = 0
+                layer.shadowPath = nil
+            } else {
+                layer.masksToBounds = false
+                layer.shadowOffset = CGSize(width: 10, height: 10)
+                layer.shadowOpacity = 0.1
+                layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+            }
+        }
+    }
+}
