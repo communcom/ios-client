fe625b9e0c7f4c6b312db75963bd6af3624c4eae
Chung Tran <bigearsenal@gmail.com>
2019-04-18 11:16:13 +0300

    separate logic

--- Commun/Controllers/ProfilePageVC/ProfilePageVC.swift
+++ Commun/Controllers/ProfilePageVC/ProfilePageVC.swift
@@ -40,49 +40,16 @@ class ProfilePageVC: UIViewController {
     }
     
     func bindViewModel() {
+        // retrieve profile
         viewModel.profile
-            .asObservable()
             .filter {$0 != nil}
             .map {$0!}
             .subscribe(onNext: {profile in
                 self.showProfile(profile)
             })
             .disposed(by: bag)
-    }
-    
-    func showProfile(_ profile: ResponseAPIContentGetProfile) {
-        self.activityIndicator.stopAnimating()
-        self.tableView.isHidden = false
         
-        // profile image
-        if let avatarUrl = profile.personal.avatarUrl {
-            userAvatarImage.sd_setImage(with: URL(string: avatarUrl)) { (_, error, _, _) in
-                if (error != nil) {
-                    // TODO: Placeholder image
-                    self.userAvatarImage.setImageWith(profile.username, color: #colorLiteral(red: 0.9997546077, green: 0.6376479864, blue: 0.2504218519, alpha: 1))
-                }
-            }
-        } else {
-            // TODO: Placeholder image
-            self.userAvatarImage.setImageWith(profile.username, color: #colorLiteral(red: 0.9997546077, green: 0.6376479864, blue: 0.2504218519, alpha: 1))
-        }
-        
-        // user name
-        userNameLabel.text = profile.username
-        
-        // join date
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .long
-        let dateString = dateFormatter.string(from: Date.from(string: profile.registration.time))
-        joinedDateLabel.text = ("Joined".localized() + " " + dateString)
-        
-        // count labels
-        followingsCountLabel.text = "\(profile.subscriptions.userIds.count)"
-        communitiesCountLabel.text = "\(profile.subscriptions.communities.count)"
-        #warning("missing followers count")
-        
-        
-        
+        // TODO: Bind table view
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
