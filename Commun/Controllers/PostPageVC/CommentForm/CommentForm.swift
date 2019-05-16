07cac67e7fda4b27355ece9591962f208b105d9c
Chung Tran <bigearsenal@gmail.com>
2019-05-16 19:28:09 +0300

    add commentForm

---
+++ Commun/Controllers/PostPageVC/CommentForm/CommentForm.swift
@@ -0,0 +1,33 @@
+//
+//  CommentForm.swift
+//  Commun
+//
+//  Created by Chung Tran on 16/05/2019.
+//  Copyright © 2019 Maxim Prigozhenkov. All rights reserved.
+//
+
+import UIKit
+
+class CommentForm: UIView {
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var textView: ExpandableTextView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        // init view
+        Bundle.main.loadNibNamed("CommentForm", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+}
