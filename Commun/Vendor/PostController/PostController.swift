c51a7ad918a7dae6d018d45d1e4775fb29db89e9
Chung Tran <bigearsenal@gmail.com>
2019-05-20 13:07:53 +0300

    refactor code, disable alternative button when upvote or downvote triggered

--- Commun/Vendor/PostController/PostController.swift
+++ Commun/Vendor/PostController/PostController.swift
@@ -19,6 +19,31 @@ protocol PostController: class {
 
 extension PostController {
     
+    func setHasVote(_ value: Bool, for type: VoteType) {
+        guard let post = post else {return}
+        
+        // return if nothing changes
+        if type == .upvote && value == post.votes.hasUpVote {return}
+        if type == .downvote && value == post.votes.hasDownVote {return}
+        
+        // Image names
+        let unselectedImage = type == .upvote ? "Up": "Down"
+        let selectedImage = unselectedImage + "Selected"
+        
+        // set image
+        let newImage = value ? selectedImage: unselectedImage
+        
+        if type == .upvote {
+            upVoteButton.setImage(UIImage(named: newImage), for: .normal)
+            self.post!.votes.hasUpVote = !self.post!.votes.hasUpVote
+        }
+        
+        if type == .downvote {
+            downVoteButton.setImage(UIImage(named: newImage), for: .normal)
+            self.post!.votes.hasDownVote = !self.post!.votes.hasDownVote
+        }
+    }
+    
     func openMorePostActions() {
         guard let post = post,
             let topController = UIApplication.topViewController() else {return}
@@ -27,45 +52,76 @@ extension PostController {
     
     func upVote() {
         guard let post = post else {return}
-        let originImage = post.votes.hasUpVote ? "UpSelected" : "Up"
-        let newImage = originImage == "Up" ? "UpSelected": "Up"
-        upVoteButton.setImage(UIImage(named: newImage), for: .normal)
         
-        self.post!.votes.hasUpVote = !self.post!.votes.hasUpVote
+        // save original state
+        let originHasUpVote = post.votes.hasUpVote
+        let originHasDownVote = post.votes.hasDownVote
         
+        // change state
+        setHasVote(originHasUpVote ? false: true, for: .upvote)
+        setHasVote(false, for: .downvote)
+        
+        // disable button until transaction is done
         upVoteButton.isEnabled = false
+        downVoteButton.isEnabled = false
+        
+        // send request
         upVoteObserver(post)
             .subscribe(
                 onCompleted: {
+                    // re-enable buttons
                     self.upVoteButton.isEnabled = true
+                    self.downVoteButton.isEnabled = true
                 },
                 onError: {_ in
-                    self.post!.votes.hasUpVote = !self.post!.votes.hasUpVote
-                    UIApplication.topViewController()?.showGeneralError()
-                    self.upVoteButton.setImage(UIImage(named: originImage), for: .normal)
+                    // reset state
+                    self.setHasVote(originHasUpVote, for: .upvote)
+                    self.setHasVote(originHasDownVote, for: .downvote)
+                    
+                    // re-enable buttons
                     self.upVoteButton.isEnabled = true
+                    self.downVoteButton.isEnabled = true
+                    
+                    // show general error
+                    UIApplication.topViewController()?.showGeneralError()
             })
             .disposed(by: disposeBag)
     }
     
     func downVote() {
         guard let post = post else {return}
-        let originImage = post.votes.hasDownVote ? "DownSelected" : "Down"
-        let newImage = originImage == "Down" ? "DownSelected": "Down"
-        downVoteButton.setImage(UIImage(named: newImage), for: .normal)
-        self.post!.votes.hasDownVote = !self.post!.votes.hasDownVote
         
+        // save original state
+        let originHasUpVote = post.votes.hasUpVote
+        let originHasDownVote = post.votes.hasDownVote
+        
+        // change state
+        setHasVote(originHasDownVote ? false: true, for: .downvote)
+        setHasVote(false, for: .upvote)
+        
+        // disable button until transaction is done
+        upVoteButton.isEnabled = false
         downVoteButton.isEnabled = false
+        
+        // send request
         downVoteObserver(post)
             .subscribe(
                 onCompleted: {
+                    // re-enable buttons
+                    self.upVoteButton.isEnabled = true
                     self.downVoteButton.isEnabled = true
                 },
                 onError: {_ in
-                    self.post!.votes.hasDownVote = !self.post!.votes.hasDownVote
-                    UIApplication.topViewController()?.showGeneralError()
-                    self.downVoteButton.setImage(UIImage(named: originImage), for: .normal)
+                    // reset state
+                    self.setHasVote(originHasUpVote, for: .upvote)
+                    self.setHasVote(originHasDownVote, for: .downvote)
+                    
+                    // re-enable buttons
+                    self.upVoteButton.isEnabled = true
                     self.downVoteButton.isEnabled = true
+                    
+                    // show general error
+                    UIApplication.topViewController()?.showGeneralError()
             })
             .disposed(by: disposeBag)
     }
