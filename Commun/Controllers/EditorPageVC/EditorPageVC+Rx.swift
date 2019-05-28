25d49f1652010403b00c5db3b5ba0c308ac28097
Chung Tran <bigearsenal@gmail.com>
2019-05-28 14:09:22 +0300

    refactor code

--- Commun/Controllers/EditorPageVC/EditorPageVC+Rx.swift
+++ Commun/Controllers/EditorPageVC/EditorPageVC+Rx.swift
@@ -22,16 +22,17 @@ extension EditorPageVC {
             .disposed(by: disposeBag)
         
         // verification
-        let titleContentCombined = Observable.combineLatest(titleTextField.rx.text.orEmpty, contentTextView.rx.text.orEmpty).share()
         
         #warning("Verify community")
         #warning("fix contentText later")
-        titleContentCombined.map {
+        Observable.combineLatest(titleTextField.rx.text.orEmpty, contentTextView.rx.text.orEmpty)
+            .map {
                 // Text field  is not empty
                 (!$0.0.isEmpty) && (!$0.1.isEmpty) &&
                 // Title or content has changed
                 ($0.0 != viewModel.postForEdit?.content.title ||
-                $0.1 != viewModel.postForEdit?.content.body.preview)}
+                $0.1 != viewModel.postForEdit?.content.body.preview)
+            }
             .bind(to: sendPostButton.rx.isEnabled)
             .disposed(by: disposeBag)
     }
