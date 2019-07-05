3aa5173ec6b0ecbdc4a82e84562915a119b94bfa
Chung Tran <bigearsenal@gmail.com>
2019-07-05 20:11:41 +0300

    completed creating bio

--- Commun/Controllers/SignUpScenes/BoardingVCs/CreateBioVC.swift
+++ Commun/Controllers/SignUpScenes/BoardingVCs/CreateBioVC.swift
@@ -11,11 +11,45 @@ import RxSwift
 
 class CreateBioVC: UIViewController, SignUpRouter {
     let disposeBag = DisposeBag()
+    @IBOutlet weak var textView: ExpandableTextView!
+    @IBOutlet weak var characterCountLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    private let charactersLimit = 100
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        bindUI()
+    }
+    
+    func bindUI() {
+        // Bind textView
+        let bioChanged = textView.rx.text.orEmpty
+            .map {$0.count != 0}
+        
+        bioChanged
+            .subscribe(onNext: {changed in
+                self.nextButton.isEnabled = changed
+                self.nextButton.backgroundColor = changed ? #colorLiteral(red: 0.4173236787, green: 0.5017360449, blue: 0.9592832923, alpha: 1) : #colorLiteral(red: 0.7063884139, green: 0.749147296, blue: 0.9795948863, alpha: 1)
+            })
+            .disposed(by: disposeBag)
+        
+        textView.rx.text.orEmpty
+            .subscribe(onNext: {text in
+                self.characterCountLabel.text = "\(text.count)/100"
+            })
+            .disposed(by: disposeBag)
+        
+        textView.rx.text.orEmpty
+            .filter {$0.count > self.charactersLimit}
+            .subscribe(onNext: limitCharacters())
+            .disposed(by: disposeBag)
+    }
+    
+    func limitCharacters() -> (_ newText: String) -> Void {
+        return { newText in
+            self.textView.text = String(newText.prefix(self.charactersLimit))
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,10 +63,15 @@ class CreateBioVC: UIViewController, SignUpRouter {
     }
     
     @IBAction func nextButtonDidTouch(_ sender: Any) {
+        guard let bio = textView.text else {return}
         
-        // TODO: save bio
-        
-        endSigningUp()
+        NetworkService.shared.updateMeta(params: ["about": bio])
+            .subscribe(onCompleted: {
+                self.endSigningUp()
+            }) { (error) in
+                self.showError(error)
+            }
+            .disposed(by: disposeBag)
     }
     
     @IBAction func skipButtonDidTouch(_ sender: Any) {
