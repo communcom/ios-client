023e2df2dec148430a82f88909c2df2616371bac
Chung Tran <bigearsenal@gmail.com>
2019-07-19 15:32:40 +0700

    add action for buttons

--- Commun/Controllers/SettingsVC/Cell/SettingsButtonCell.swift
+++ Commun/Controllers/SettingsVC/Cell/SettingsButtonCell.swift
@@ -20,17 +20,50 @@ class SettingsButtonCell: UITableViewCell {
     
     @IBOutlet weak var button: UIButton!
     var delegate: SettingsButtonCellDelegate?
+    var type: ButtonType?
     
     func setUpWithButtonType(_ type: ButtonType) {
+        self.type = type
         switch type {
         case .changeAllPassword:
             button.setTitle("Change all password".localized(), for: .normal)
+            button.setTitleColor(.appMainColor, for: .normal)
         case .logout:
             button.setTitle("Log out".localized(), for: .normal)
+            button.setTitleColor(.red, for: .normal)
         }
     }
     
     @IBAction func changePasswordButtonTap(_ sender: Any) {
+        guard let type = type else {return}
+        switch type {
+        case .changeAllPassword:
+            let alert = UIAlertController(title: "Change all password",
+                                          message: "Changing passwords will save your wallet if someone saw your password.",
+                                          preferredStyle: .alert)
+            alert.addTextField { field in
+                field.placeholder = "Paste owner password"
+            }
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { _ in
+                print("Update password")
+            }))
+            
+            parentViewController?.present(alert, animated: true, completion: nil)
+        case .logout:
+            parentViewController?.showAlert(title: "Logout".localized(), message: "Do you really want to logout?".localized(), buttonTitles: ["Ok".localized(), "Cancel".localized()], highlightedButtonIndex: 1) { (index) in
+                
+                if index == 0 {
+                    do {
+                        try CurrentUser.logout()
+                        AppDelegate.reloadSubject.onNext(true)
+                    } catch {
+                        self.parentViewController?.showError(error)
+                    }
+                }
+            }
+        }
         delegate?.buttonDidTap(on: self)
     }
 }
