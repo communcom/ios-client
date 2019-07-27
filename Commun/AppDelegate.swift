efa40e732c7b0b31fa889b27edd5a9caee2d45dd
Chung Tran <bigearsenal@gmail.com>
2019-07-27 18:28:02 +0700

    - update libs - manage socket life cycle

--- Commun/AppDelegate.swift
+++ Commun/AppDelegate.swift
@@ -181,8 +181,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pau ]]]]]]]]se the game.
+        SocketManager.shared.disconnect()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -193,6 +192,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        SocketManager.shared.connect()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -200,8 +200,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
+        SocketManager.shared.disconnect()
         self.saveContext()
     }
     
