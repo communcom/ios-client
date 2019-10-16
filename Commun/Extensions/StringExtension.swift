ef806f6be8394b6bce6c075727234b39ded9858b
Chung Tran <bigearsenal@gmail.com>
2019-10-16 18:22:23 +0700

    remove unused functions

--- Commun/Extensions/StringExtension.swift
+++ Commun/Extensions/StringExtension.swift
@@ -14,21 +14,6 @@ extension String {
 //        return "\u{2063}"
 //    }
     
-    func md5() -> String? {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        var digest = [UInt8](repeating: 0, count: length)
-        
-        if let d = self.data(using: String.Encoding.utf8) {
-            _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-                CC_MD5(body, CC_LONG(d.count), &digest)
-            }
-        }
-        
-        return (0..<length).reduce("") {
-            $0 + String(format: "%02x", digest[$1])
-        }
-    }
-    
     func getTags() -> [String] {
         if let regex = try? NSRegularExpression(pattern: NSRegularExpression.tagRegexPattern, options: .caseInsensitive)
         {
@@ -51,36 +36,6 @@ extension String {
     
     var isLink: Bool {
         return self.matches("^\(NSRegularExpression.linkRegexPattern)$")
-    }
-    
-//    func highlightMentionAttributedString() -> NSAttributedString {
-//        let attributed = NSMutableAttributedString(string: self)
-//        if let regex = try? NSRegularExpression(pattern: "\\s" + .tagRegex, options: .caseInsensitive){
-//            for match in regex.matches(in: self, range: NSRange(location: 0, length: self.utf16.count)) as [NSTextCheckingResult] {
-//                attributed.addAttribute(.foregroundColor, value: UIColor.appMainColor, range: match.range)
-//                attributed.addAttribute(.font, value: UIFont.systemFont(ofSize: 15, weight: .semibold), range: match.range)
-//            }
-//        }
-//        if let regex = try? NSRegularExpression(pattern: .mentionRegex, options: .caseInsensitive){
-//            for match in regex.matches(in: self, range: NSRange(location: 0, length: self.utf16.count)) as [NSTextCheckingResult] {
-//                attributed.addAttribute(.foregroundColor, value: UIColor.appMainColor, range: match.range)
-//                attributed.addAttribute(.font, value: UIFont.systemFont(ofSize: 15, weight: .semibold), range: match.range)
-//            }
-//        }
-//        return attributed
-//    }
-    
-    func getJsonMetadata() -> [[String: String]] {
-        var embeds = [[String: String]]()
-        
-        for word in components(separatedBy: " ") {
-            if word.contains("http://") || word.contains("https://") {
-                if embeds.first(where: {$0["url"] == word}) != nil {continue}
-                #warning("Define type")
-                embeds.append(["url": word])
-            }
-        }
-        return embeds
     }
     
     func matches(_ regex: String) -> Bool {
