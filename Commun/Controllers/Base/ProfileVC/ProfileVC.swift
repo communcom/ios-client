0b97b2962a656ec35991fe1f42764dd6c2b6207e
Chung Tran <bigearsenal@gmail.com>
2019-10-28 18:47:37 +0700

    fix headerView

--- Commun/Controllers/Base/ProfileVC/ProfileVC.swift
+++ Commun/Controllers/Base/ProfileVC/ProfileVC.swift
@@ -144,4 +144,21 @@ class ProfileVC<ProfileType: Decodable>: BaseViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        if let headerView = tableView.tableHeaderView {
+
+            let height = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+            var headerFrame = headerView.frame
+
+            //Comparison necessary to avoid infinite loop
+            if height != headerFrame.size.height {
+                headerFrame.size.height = height
+                headerView.frame = headerFrame
+                tableView.tableHeaderView = headerView
+            }
+        }
+    }
 }
