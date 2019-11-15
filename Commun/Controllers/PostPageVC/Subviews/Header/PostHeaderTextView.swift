d39bf10920b7d88e6df4083bb8a9c49afa97324b
Chung Tran <bigearsenal@gmail.com>
2019-11-15 09:44:59 +0700

    Refactor: MySubviewAttachingTextView

--- Commun/Controllers/PostPageVC/Subviews/Header/PostHeaderTextView.swift
+++ Commun/Controllers/PostPageVC/Subviews/Header/PostHeaderTextView.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SubviewAttachingTextView
 
 class PostHeaderTextView: MySubviewAttachingTextView {
     static let attachmentInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
@@ -39,69 +38,3 @@ class PostHeaderTextView: MySubviewAttachingTextView {
     }
 }
 
-open class MySubviewAttachingTextView: UITextView {
-
-    public override init(frame: CGRect, textContainer: NSTextContainer?) {
-        super.init(frame: frame, textContainer: textContainer)
-        self.commonInit()
-    }
-
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.commonInit()
-    }
-
-    private let attachmentBehavior = MySubviewAttachingTextViewBehavior()
-
-    func commonInit() {
-        // Connect the attachment behavior
-        self.attachmentBehavior.textView = self
-        self.layoutManager.delegate = self.attachmentBehavior
-        self.textStorage.delegate = self.attachmentBehavior
-    }
-
-    open override var textContainerInset: UIEdgeInsets {
-        didSet {
-            // Text container insets are used to convert coordinates between the text container and text view, so a change to these insets must trigger a layout update
-            self.attachmentBehavior.layoutAttachedSubviews()
-        }
-    }
-
-}
-
-class MySubviewAttachingTextViewBehavior: SubviewAttachingTextViewBehavior {
-    override func layoutAttachedSubviews() {
-        guard let textView = self.textView else {
-            return
-        }
-
-        let layoutManager = textView.layoutManager
-        let scaleFactor = textView.window?.screen.scale ?? UIScreen.main.scale
-
-        // For each attached subview, find its associated attachment and position it according to its text layout
-        let attachmentRanges = textView.textStorage.subviewAttachmentRanges
-        for (attachment, range) in attachmentRanges {
-            guard let view = self.attachedViews.object(forKey: attachment.viewProvider) else {
-                // A view for this provider is not attached yet??
-                continue
-            }
-            guard view.superview === textView else {
-                // Skip views which are not inside the text view for some reason
-                continue
-            }
-            guard let attachmentRect = SubviewAttachingTextViewBehavior.boundingRect(forAttachmentCharacterAt: range.location, layoutManager: layoutManager) else {
-                // Can't determine the rectangle for the attachment: just hide it
-                view.isHidden = true
-                continue
-            }
-
-            let integralRect = CGRect(origin: attachmentRect.origin.integral(withScaleFactor: scaleFactor),
-                                      size: attachmentRect.size)
-            
-            UIView.performWithoutAnimation {
-                view.frame = integralRect
-                view.isHidden = false
-            }
-        }
-    }
-}
