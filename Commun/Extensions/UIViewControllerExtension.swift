66397f276632162dbd92b574f21752f7d420e41f
Chung Tran <bigearsenal@gmail.com>
2019-11-19 17:06:46 +0700

    fix showing hud

--- Commun/Extensions/UIViewControllerExtension.swift
+++ Commun/Extensions/UIViewControllerExtension.swift
@@ -77,13 +77,13 @@ extension UIViewController {
     }
     
     func hideHud() {
-        let vc = navigationController ?? self
+        let vc = tabBarController ?? navigationController ?? self
         
         MBProgressHUD.hide(for: vc.view, animated: false)
     }
     
     func showIndetermineHudWithMessage(_ message: String) {
-        let vc = navigationController ?? self
+        let vc = tabBarController ?? navigationController ?? self
         
         // Hide all previous hud
         hideHud()
@@ -98,7 +98,7 @@ extension UIViewController {
     }
     
     func showDone(_ message: String, completion: (()->Void)? = nil) {
-        let vc = navigationController ?? self
+        let vc = tabBarController ?? navigationController ?? self
         
         // Hide all previous hud
         hideHud()
