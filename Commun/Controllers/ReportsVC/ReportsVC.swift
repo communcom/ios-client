c3c54b225aac136a1809b9064497e61431aa3673
Chung Tran <bigearsenal@gmail.com>
2019-11-25 10:58:25 +0700

    fix layout for ReportsVC

--- Commun/Controllers/ReportsVC/ReportsVC.swift
+++ Commun/Controllers/ReportsVC/ReportsVC.swift
@@ -34,6 +34,35 @@ class ReportsVC: BaseVerticalStackViewController {
         closeButton.addTarget(self, action: #selector(back), for: .touchUpInside)
     }
     
+    override func layout() {
+        stackView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16), excludingEdge: .bottom)
+        
+        let alertView = UIView(height: 62, backgroundColor: .white, cornerRadius: 10)
+        scrollView.contentView.addSubview(alertView)
+        alertView.autoPinEdge(.top, to: .bottom, of: stackView, withOffset: 20)
+        alertView.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
+        alertView.autoPinEdge(toSuperviewEdge: .trailing, withInset: 16)
+        
+        let imageView = UIImageView(width: 24, height: 24)
+        imageView.image = UIImage.init(named: "reports-alert")
+        alertView.addSubview(imageView)
+        imageView.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
+        imageView.autoAlignAxis(toSuperviewAxis: .horizontal)
+        
+        let descriptionLabel = UILabel.with(text: "if someone is in immediate danger, call local emergency services. Don't wait.".localized().uppercaseFirst, textSize: 12, weight: .semibold, textColor: .a5a7bd, numberOfLines: 2)
+        alertView.addSubview(descriptionLabel)
+        descriptionLabel.autoPinEdge(.leading, to: .trailing, of: imageView, withOffset: 10)
+        descriptionLabel.autoAlignAxis(toSuperviewAxis: .horizontal)
+        descriptionLabel.autoPinEdge(toSuperviewEdge: .trailing, withInset: 16)
+        
+        let sendButton = CommunButton.default(height: 50, label: "send".localized().uppercaseFirst)
+        scrollView.contentView.addSubview(sendButton)
+        sendButton.autoPinEdge(.top, to: .bottom, of: alertView, withOffset: 30)
+        sendButton.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
+        sendButton.autoPinEdge(toSuperviewEdge: .trailing, withInset: 16)
+        sendButton.autoPinEdge(toSuperviewEdge: .bottom, withInset: 10)
+    }
+    
     override func viewForAction(_ action: BaseVerticalStackViewController.Action) -> UIView {
         let actionView = ReportOptionView(height: 58, backgroundColor: .white)
         actionView.checkBox.isUserInteractionEnabled = false
