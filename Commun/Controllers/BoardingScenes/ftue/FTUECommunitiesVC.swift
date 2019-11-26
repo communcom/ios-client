a15ece6767748f12588464d2e6893ceb35fa7b8c
Chung Tran <bigearsenal@gmail.com>
2019-11-26 10:06:35 +0700

    add headerView

--- Commun/Controllers/BoardingScenes/ftue/FTUECommunitiesVC.swift
+++ Commun/Controllers/BoardingScenes/ftue/FTUECommunitiesVC.swift
@@ -13,6 +13,7 @@ class FTUECommunitiesVC: BaseViewController {
     let bottomBarHeight: CGFloat = 114
     
     // MARK: - Properties
+    lazy var headerView = UIView(forAutoLayout: ())
     lazy var searchBar: UISearchBar = {
         let searchBar = UISearchBar(forAutoLayout: ())
         searchBar.searchBarStyle = .minimal
@@ -27,10 +28,13 @@ class FTUECommunitiesVC: BaseViewController {
     // MARK: - Methods
     override func setUp() {
         super.setUp()
+        // headerView
+        view.addSubview(headerView)
+        headerView.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .bottom)
         
         // titleLabel
         let titleLabel = UILabel.with(text: "get you first points".localized().uppercaseFirst, textSize: 33, weight: .bold, numberOfLines: 0)
-        view.addSubview(titleLabel)
+        headerView.addSubview(titleLabel)
         titleLabel.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 10, left: 16, bottom: 0, right: 16), excludingEdge: .bottom)
         
         // descriptionLabel
@@ -39,16 +43,15 @@ class FTUECommunitiesVC: BaseViewController {
         paragraphStyle.lineSpacing = 10
         let attrString = NSAttributedString(string: "subscribe to at least 3 communities and get your first Community Points".localized().uppercaseFirst, attributes: [.paragraphStyle: paragraphStyle])
         descriptionLabel.attributedText = attrString
-        view.addSubview(descriptionLabel)
+        headerView.addSubview(descriptionLabel)
         descriptionLabel.autoPinEdge(.top, to: .bottom, of: titleLabel, withOffset: 16)
         descriptionLabel.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
         descriptionLabel.autoPinEdge(toSuperviewEdge: .trailing, withInset: 16)
         
         // searchBar
-        view.addSubview(searchBar)
+        headerView.addSubview(searchBar)
         searchBar.autoPinEdge(.top, to: .bottom, of: descriptionLabel, withOffset: 25)
-        searchBar.autoPinEdge(toSuperviewEdge: .leading, withInset: 10)
-        searchBar.autoPinEdge(toSuperviewEdge: .trailing, withInset: 10)
+        searchBar.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(inset: 10), excludingEdge: .top)
         
         // collection view
         
