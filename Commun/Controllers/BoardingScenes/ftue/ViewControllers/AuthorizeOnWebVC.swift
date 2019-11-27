09b87cb067461f03c232a24096fb50e546cd88ef
Chung Tran <bigearsenal@gmail.com>
2019-11-27 11:27:19 +0700

    fix layout for iphone 5s

--- Commun/Controllers/BoardingScenes/ftue/ViewControllers/AuthorizeOnWebVC.swift
+++ Commun/Controllers/BoardingScenes/ftue/ViewControllers/AuthorizeOnWebVC.swift
@@ -25,22 +25,22 @@ class AuthorizeOnWebVC: BaseViewController, BoardingRouter {
         view.addSubview(imageView)
         imageView.autoPinEdge(toSuperviewEdge: .top, withInset: 10)
         imageView.autoAlignAxis(toSuperviewAxis: .vertical)
-        imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/2 * Config.heightRatio)
+        imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/2)
             .isActive = true
         imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor)
             .isActive = true
         
-        let label1 = UILabel.with(text: "authorize on web".localized().uppercaseFirst, textSize: 33, weight: .bold)
+        let label1 = UILabel.with(text: "authorize on web".localized().uppercaseFirst, textSize: 33 * Config.heightRatio, weight: .bold)
         view.addSubview(label1)
-        label1.autoPinEdge(.top, to: .bottom, of: imageView, withOffset: 27)
+        label1.autoPinEdge(.top, to: .bottom, of: imageView, withOffset: 27 * Config.heightRatio)
         label1.autoAlignAxis(toSuperviewAxis: .vertical)
         
-        let label2 = UILabel.with(text: "triple your points".localized().uppercaseFirst, textSize: 33)
+        let label2 = UILabel.with(text: "triple your points".localized().uppercaseFirst, textSize: 33 * Config.heightRatio)
         view.addSubview(label2)
         label2.autoPinEdge(.top, to: .bottom, of: label1, withOffset: 4)
         label2.autoAlignAxis(toSuperviewAxis: .vertical)
         
-        let label3 = UILabel.with(text: "right after you authorize on the Website\nWe triple your welcome points".localized().uppercaseFirst, textSize: 17, textColor: .a5a7bd, numberOfLines: 0, textAlignment: .center)
+        let label3 = UILabel.with(text: "right after you authorize on the Website\nWe triple your welcome points".localized().uppercaseFirst, textSize: 17 * Config.heightRatio, textColor: .a5a7bd, numberOfLines: 0, textAlignment: .center)
         view.addSubview(label3)
         label3.autoPinEdge(.top, to: .bottom, of: label2, withOffset: 20)
         label3.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
