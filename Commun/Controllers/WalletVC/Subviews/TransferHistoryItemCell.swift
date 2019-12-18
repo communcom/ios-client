b0a4fce063cb77439ee5074bbb1462493477428c
Chung Tran <bigearsenal@gmail.com>
2019-12-18 16:29:25 +0700

    configure cell

--- Commun/Controllers/WalletVC/Subviews/TransferHistoryItemCell.swift
+++ Commun/Controllers/WalletVC/Subviews/TransferHistoryItemCell.swift
@@ -57,29 +57,59 @@ class TransferHistoryItemCell: MyTableViewCell, ListItemCellType {
         var memo: NSAttributedString
         
         switch item.meta.actionType {
-        case "transaction":
+        case "transfer":
             var avatarUrl: String?
             if item.meta.direction == "send" {
                 avatarUrl = item.receiver.avatarUrl
                 username = item.receiver.username ?? item.receiver.userId
                 memo = NSMutableAttributedString()
-                    .semibold("-\(item.memo)", font: .systemFont(ofSize: 12, weight: .semibold))
+                    .semibold("-\(item.quantity) Commun", font: .systemFont(ofSize: 15, weight: .semibold))
             } else {
                 avatarUrl = item.sender.avatarUrl
                 username = item.sender.username ?? item.sender.userId
                 memo = NSMutableAttributedString()
-                    .semibold("+\(item.memo)", font: .systemFont(ofSize: 12, weight: .semibold), color: .plus)
+                    .semibold("+\(item.quantity) Commun", font: .systemFont(ofSize: 15, weight: .semibold), color: .plus)
             }
             
             avatarImageView.setAvatar(urlString: avatarUrl, namePlaceHolder: username)
             
             iconImageView.isHidden = false
-            iconImageView.sd_setImage(with: URL(string: item.point.logo ?? ""), placeholderImage: UIImage(color: .appMainColor))
+            iconImageView.image = UIImage(named: "tux")
             
+        case "convert":
+            username = "refill".localized().uppercaseFirst
+            if item.meta.transferType == "token" {
+                memo = NSMutableAttributedString()
+                    .semibold("+\(item.meta.exchangeAmount ?? 0) \(item.point.name!)", font: .systemFont(ofSize: 15, weight: .semibold), color: .plus)
+                iconImageView.isHidden = false
+                avatarImageView.setAvatar(urlString: item.point.logo, namePlaceHolder: item.point.name ?? "C")
+                iconImageView.image = UIImage(named: "tux")
+            } else {
+                memo = NSMutableAttributedString()
+                    .semibold("+\(item.meta.exchangeAmount ?? 0) Commun", font: .systemFont(ofSize: 15, weight: .semibold), color: .plus)
+                iconImageView.isHidden = false
+                iconImageView.sd_setImage(with: URL(string: item.point.logo ?? ""), placeholderImage: UIImage(color: .appMainColor))
+                avatarImageView.image = UIImage(named: "tux")
+            }
+        case "reward":
+            username = "reward".localized().uppercaseFirst
+            memo = NSMutableAttributedString()
+                .semibold("+\(item.quantity) \(item.point.name!)", font: .systemFont(ofSize: 15, weight: .semibold), color: .plus)
+            
+            avatarImageView.setAvatar(urlString: item.point.logo, namePlaceHolder: username)
+            iconImageView.isHidden = true
+        case "hold":
+            username = item.meta.holdType?.localized().uppercaseFirst ?? ""
+            memo = NSMutableAttributedString()
+                .semibold("+\(item.quantity) \(item.point.name!)", font: .systemFont(ofSize: 15, weight: .semibold), color: .plus)
+            
+            avatarImageView.image = UIImage(named: "wallet-like")
+            iconImageView.isHidden = true
         default:
             // TODO: - Other types
             username = ""
             memo = NSMutableAttributedString()
+            iconImageView.isHidden = true
             break
         }
         
