7548006b9b22686c7d47ac3423dbd8f9753d99ef
Chung Tran <bigearsenal@gmail.com>
2019-12-26 14:03:15 +0700

    Prevent duplicating requests

--- Commun/Controllers/WalletVC/ViewControllers/WalletConvertVCs/WalletConvertViewModel.swift
+++ Commun/Controllers/WalletVC/ViewControllers/WalletConvertVCs/WalletConvertViewModel.swift
@@ -14,12 +14,27 @@ class WalletConvertViewModel: BalancesViewModel {
     let buyPrice = BehaviorRelay<Double>(value: 0)
     let sellPrice = BehaviorRelay<Double>(value: 0)
     
+    // Prevent duplicating
+    private var currentBuyPriceSymbol: String?
+    private var currentBuyPriceQuantity: String?
+    private var currentSellPriceQuantity: String?
+    
     func getBuyPrice(symbol: String, quantity: String) {
+        // save current (for comparison
+        currentBuyPriceSymbol = symbol
+        currentBuyPriceQuantity = quantity
+        
+        // set state
         priceLoadingState.accept(.loading)
         RestAPIManager.instance.getBuyPrice(symbol: symbol, quantity: quantity)
             .subscribe(onSuccess: { [weak self] result in
                 self?.priceLoadingState.accept(.finished)
-                self?.buyPrice.accept(result.priceValue)
+                // prevent duplicating
+                if result.symbol == self?.currentBuyPriceSymbol && result.quantity == self?.currentBuyPriceQuantity
+                {
+                    self?.buyPrice.accept(result.priceValue)
+                }
+                
             }, onError: { [weak self] (error) in
                 self?.priceLoadingState.accept(.error(error: error))
             })
@@ -27,11 +42,18 @@ class WalletConvertViewModel: BalancesViewModel {
     }
     
     func getSellPrice(quantity: String) {
+        // save current (for comparison
+        currentSellPriceQuantity = quantity
+        
+        // set state
         priceLoadingState.accept(.loading)
         RestAPIManager.instance.getSellPrice(quantity: quantity)
             .subscribe(onSuccess: { [weak self] result in
                 self?.priceLoadingState.accept(.finished)
-                self?.sellPrice.accept(result.priceValue)
+                // prevent duplicating
+                if result.quantity == self?.currentSellPriceQuantity {
+                    self?.sellPrice.accept(result.priceValue)
+                }
             }, onError: { [weak self] (error) in
                 self?.priceLoadingState.accept(.error(error: error))
             })
