7643862ec890e3339b19623377cb2eb450e54995
Chung Tran <bigearsenal@gmail.com>
2019-12-27 15:25:34 +0700

    Fix carousel in WalletBuyCommunVC

--- Commun/Controllers/WalletVC/ViewControllers/WalletConvertVCs/WalletBuyCommunVC.swift
+++ Commun/Controllers/WalletVC/ViewControllers/WalletConvertVCs/WalletBuyCommunVC.swift
@@ -8,17 +8,19 @@
 
 import Foundation
 import RxSwift
-import CircularCarousel
 
 class WalletBuyCommunVC: WalletConvertVC {
-    lazy var carousel = CircularCarousel(width: 300, height: 50)
-    var carouselItems = [ResponseAPIWalletGetBalance]()
+    lazy var carousel = WalletCarousel(height: 50)
     
     override func setUp() {
         super.setUp()
         buyNameLabel.text = "Commun"
         buyLogoImageView.image = UIImage(named: "tux")
         convertBuyLabel.text = "buy".localized().uppercaseFirst + " Commun"
+        
+        carousel.scrollingHandler = { index in
+            self.currentBalance = self.viewModel.items.value[safe: index + 1]
+        }
     }
     
     override func bind() {
@@ -26,16 +28,14 @@ class WalletBuyCommunVC: WalletConvertVC {
         viewModel.items
             .map {$0.filter {$0.symbol != "CMN"}}
             .subscribe(onNext: { (items) in
-                self.carouselItems = items
+                self.carousel.balances = items
+                self.carousel.currentIndex = items.firstIndex(where: {$0.symbol == self.currentSymbol}) ?? 0
                 self.carousel.reloadData()
             })
             .disposed(by: disposeBag)
     }
     
     override func layoutCarousel() {
-        carousel.delegate = self
-        carousel.dataSource = self
-        
         scrollView.addSubview(carousel)
         carousel.autoPinEdge(toSuperviewEdge: .top, withInset: 20)
         carousel.autoAlignAxis(toSuperviewAxis: .vertical)
@@ -169,58 +169,5 @@ class WalletBuyCommunVC: WalletConvertVC {
                 self.showError(error)
             }
             .disposed(by: disposeBag)
-    }
-}
-
-extension WalletBuyCommunVC: CircularCarouselDataSource, CircularCarouselDelegate {
-    func startingItemIndex(inCarousel carousel: CircularCarousel) -> Int {
-        return (carouselItems.firstIndex(where: {$0.symbol == self.currentSymbol}) ?? 0)
-    }
-    
-    func numberOfItems(inCarousel carousel: CircularCarousel) -> Int {
-        return min(5, carouselItems.count)
-    }
-    
-    func carousel(_: CircularCarousel, viewForItemAt indexPath: IndexPath, reuseView: UIView?) -> UIView {
-        guard let balance = carouselItems[safe: indexPath.row] else {return UIView()}
-        
-        var view = reuseView
-
-        if view == nil || view?.viewWithTag(1) == nil {
-            view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-            let imageView = MyAvatarImageView(size: 50)
-            imageView.borderColor = .white
-            imageView.borderWidth = 2
-            imageView.tag = 1
-            view!.addSubview(imageView)
-            imageView.autoAlignAxis(toSuperviewAxis: .horizontal)
-            imageView.autoAlignAxis(toSuperviewAxis: .vertical)
-        }
-        
-        let imageView = view?.viewWithTag(1) as! MyAvatarImageView
-    
-        imageView.setAvatar(urlString: balance.logo, namePlaceHolder: balance.name ?? balance.symbol)
-        
-        return view!
-    }
-    
-    func carousel<CGFloat>(_ carousel: CircularCarousel, valueForOption option: CircularCarouselOption, withDefaultValue defaultValue: CGFloat) -> CGFloat {
-        if option == .itemWidth {
-            return CoreGraphics.CGFloat(50) as! CGFloat
-        }
-        
-        if option == .spacing {
-            return CoreGraphics.CGFloat(8) as! CGFloat
-        }
-        
-        if option == .minScale {
-            return CoreGraphics.CGFloat(0.7) as! CGFloat
-        }
-        
-        return defaultValue
-    }
-    
-    func carousel(_ carousel: CircularCarousel, willBeginScrollingToIndex index: Int) {
-        currentBalance = viewModel.items.value[safe: index + 1]
     }
 }
