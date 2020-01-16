b18f906a838ae3ec12ac8d82f097e0ad196af7e2
Chung Tran <bigearsenal@gmail.com>
2020-01-16 17:12:30 +0700

    navigation

--- Commun/Controllers/NotificationsPageVC/NotificationsPageVC.swift
+++ Commun/Controllers/NotificationsPageVC/NotificationsPageVC.swift
@@ -148,9 +148,38 @@ class NotificationsPageVC: ListViewController<ResponseAPIGetNotificationItem, No
     }
     
     override func modelSelected(_ item: ResponseAPIGetNotificationItem) {
-        
+        switch item.eventType {
+        case "subscribe":
+            if let id = item.user?.userId {
+                showProfileWithUserId(id)
+            }
+        case "upvote", "reply":
+            switch item.entityType {
+            case "post":
+                if let userId = item.post?.contentId.userId,
+                    let permlink = item.post?.contentId.permlink,
+                    let communityId = item.post?.contentId.communityId
+                {
+                    let postVC = PostPageVC(userId: userId, permlink: permlink, communityId: communityId)
+                    show(postVC, sender: self)
+                }
+            case "comment":
+                if let userId = item.comment?.parents?.post?.userId,
+                    let permlink = item.comment?.parents?.post?.permlink,
+                    let communityId = item.comment?.parents?.post?.communityId
+                {
+                    let postVC = PostPageVC(userId: userId, permlink: permlink, communityId: communityId)
+                    show(postVC, sender: self)
+                }
+            default:
+                break
+            }
+        default:
+            break
+        }
         
         if item.isNew {
+            // TODO: - Mark as read
             var item = item
             item.isNew = false
             item.notifyChanged()
