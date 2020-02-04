22181360319c69dff1d4e714f2f1e643c6a9c524
Chung Tran <bigearsenal@gmail.com>
2020-02-04 15:40:26 +0700

    fix layout for CommunityMembersVC

--- Commun/Controllers/CommunityPageVC/CommunityMembersVC/Subviews/CommunityMembersHeaderView.swift
+++ Commun/Controllers/CommunityPageVC/CommunityMembersVC/Subviews/CommunityMembersHeaderView.swift
@@ -28,7 +28,7 @@ class CommunityMembersHeaderView: MyView {
         let label = UILabel.with(text: "leaders".localized().uppercaseFirst, textSize: 20, weight: .bold)
         addSubview(label)
         label.autoPinEdge(toSuperviewEdge: .top, withInset: 10)
-        label.autoPinEdge(toSuperviewEdge: .leading, withInset: 10)
+        label.autoPinEdge(toSuperviewEdge: .leading)
         
         leadersCollectionView.register(LeaderFollowCollectionCell.self, forCellWithReuseIdentifier: "LeaderFollowCollectionCell")
         leadersCollectionView.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
@@ -40,7 +40,7 @@ class CommunityMembersHeaderView: MyView {
         let label2 = UILabel.with(text: "members".localized().uppercaseFirst, textSize: 20, weight: .bold)
         addSubview(label2)
         label2.autoPinEdge(.top, to: .bottom, of: leadersCollectionView, withOffset: 20)
-        label2.autoPinEdge(toSuperviewEdge: .leading, withInset: 10)
+        label2.autoPinEdge(toSuperviewEdge: .leading)
         label2.autoPinEdge(toSuperviewEdge: .bottom, withInset: 16)
     }
 }
