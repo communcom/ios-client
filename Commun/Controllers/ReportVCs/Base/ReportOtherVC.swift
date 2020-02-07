7d1ded002d225d6edb53294a15d1285ed90f1dbc
Chung Tran <bigearsenal@gmail.com>
2020-02-07 16:42:14 +0700

    add textView + button

--- Commun/Controllers/ReportVCs/Base/ReportOtherVC.swift
+++ Commun/Controllers/ReportVCs/Base/ReportOtherVC.swift
@@ -11,6 +11,8 @@ import Foundation
 class ReportOtherVC: BaseViewController {
     // MARK: - Subviews
     lazy var closeButton = UIButton.close()
+    lazy var textView = UITextView(forAutoLayout: ())
+    lazy var sendButton = CommunButton.default(height: .adaptive(height: 50), label: "send".localized().uppercaseFirst, cornerRadius: .adaptive(height: 25), isHuggingContent: false, isDisableGrayColor: true)
     
     // MARK: - Methods
     override func setUp() {
@@ -19,5 +21,16 @@ class ReportOtherVC: BaseViewController {
         navigationItem.setHidesBackButton(true, animated: false)
         setRightNavBarButton(with: closeButton)
         closeButton.addTarget(self, action: #selector(back), for: .touchUpInside)
+        
+        view.addSubview(textView)
+        textView.font = UIFont.systemFont(ofSize: 17)
+        textView.placeholder = "describe the problem".localized().uppercaseFirst + "..."
+        textView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(inset: 16), excludingEdge: .bottom)
+        
+        view.addSubview(sendButton)
+        sendButton.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
+        sendButton.autoPinEdge(toSuperviewEdge: .trailing, withInset: 16)
+        sendButton.autoPinEdge(.top, to: .bottom, of: textView, withOffset: 10)
+        sendButton.autoPinBottomToSuperViewSafeAreaAvoidKeyboard(inset: 10)
     }
 }
