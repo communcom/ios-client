dfe9310df82ba5cc73416d52b32d70cb0f339ee9
Sergey Monasturskiy <msm72@mail.ru>
2020-02-21 14:16:32 +0200

    Add error case #693.

--- Commun/Controllers/WalletVC/ViewControllers/WalletConvertVCs/WalletConvertVC.swift
+++ Commun/Controllers/WalletVC/ViewControllers/WalletConvertVCs/WalletConvertVC.swift
@@ -439,6 +439,11 @@ class WalletConvertVC: BaseViewController {
     }
     
     func checkValues() -> Bool {
+        guard let errorText = errorLabel.text, errorText.isEmpty else {
+            self.hintView?.display(inPosition: convertButton.frame.origin, withType: .error(errorLabel.text!), completion: {})
+            return false
+        }
+        
         guard convertButton.isDisabled, let amount = leftTextField.text, amount.isEmpty else { return true }
         
         self.hintView?.display(inPosition: convertButton.frame.origin, withType: .enterAmount, completion: {})
