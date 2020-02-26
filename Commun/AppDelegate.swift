bed8686e6dfda4d3f173f03161334d0b20e29e98
Chung Tran <bigearsenal@gmail.com>
2020-02-26 16:26:24 +0700

    catch userNotFound

--- Commun/AppDelegate.swift
+++ Commun/AppDelegate.swift
@@ -167,9 +167,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                                 NSAttributedString.Key.font: UIFont(name: "SFProDisplay-Bold", size: .adaptive(width: 30.0))! ]
             
         case .authorizingError(let error):
-            switch error.message {
-            case "Cannot get such account from BC",
-                 _ where error.message.hasPrefix("Can't resolve name"):
+            switch error {
+            case .userNotFound:
                 AuthorizationManager.shared.status.accept(.authorizing)
                 try! RestAPIManager.instance.logout()
                 return
