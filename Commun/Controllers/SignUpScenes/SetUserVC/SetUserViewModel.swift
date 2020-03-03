e75bfb0cec9d67173375765c19672fa4150ed477
Artem Shilin <shilinartem@gmail.com>
2020-03-03 13:37:22 +0300

    761 - update username rules

--- Commun/Controllers/SignUpScenes/SetUserVC/SetUserViewModel.swift
+++ Commun/Controllers/SignUpScenes/SetUserVC/SetUserViewModel.swift
@@ -15,84 +15,67 @@ class SetUserViewModel {
     let errorMessage = BehaviorRelay<String?>(value: nil)
     
     // MARK: - Class Functions
-    func checkUserName(_ userName: String) -> [Bool] {
-        guard !userName.isEmpty else { return [false, false, false, false, false, false] }
-        // Rule 0
-        let isStartWithALetter = userName.matches("^[a-z].*$")
-        
+    func isUserNameValid(_ userName: String) -> Bool {
+        errorMessage.accept("")
+        guard !userName.isEmpty else { return false }
+
         // Rule 1
-        // • The number of characters must not exceed 32
-        // username must be between 5-32 characters
-        let isBetween5To32Characters = (userName.count >= 5 && userName.count <= 32)
+        // Uppercase letters in the username are not allowed
+        // Valid characters: letters, numbers, hyphen
+        if !userName.matches("^[a-z0-9-.]+$") {
+            errorMessage.accept("Username may contain only lower case letters, digits, dots or dashes")
+            return false
+        }
+
+        // Rule 2
+        // only lower case letters, digits, dots or dashes
+        if !userName.matches("^[a-z].*$") {
+            errorMessage.accept("Username should start with a letter")
+            return false
+        }
         
-        // Rule 2, 3, 5
-        // • Uppercase letters in the username are not allowed
-        // • Valid characters: letters, numbers, hyphen
-        
-        let containsOnlyAllowedCharacters = userName.matches("^[a-z0-9-.]+$")
-        
+        // Rule 3
+        // Username should contain at least 3 symbols
+        if userName.count < 3 {
+            errorMessage.accept("Username should contain at least 3 symbols")
+            return false
+        }
+
         // Rule 4
-        // • The hyphen character cannot be at the beginning or at the end of the username
-        let nonAlphanumericCharacterIsNotAtBeginOrEnd = !userName.starts(with: "-") && !userName.ends(with: "-") && !userName.starts(with: ".") && !userName.ends(with: ".")
+        // Username should have max 32 symbols
+        if userName.count > 32 {
+            errorMessage.accept("Username is too long. 32 symbols are maximum")
+            return false
+        }
+
+        // Rule 5
+        // Username may contain only one dash in a row
+        if userName.contains("--") {
+            errorMessage.accept("Username may contain only one dash in a row")
+            return false
+        }
 
         // Rule 6
-        // • The presence of two characters "dot" in a row is not valid
-        let twoNonAlphanumericCharacterNotSideBySide = !userName.contains("..") && !userName.contains(".-") && !userName.contains("-.") && !userName.contains("--")
-        
+        // Username may contain only one dot in a row
+        if userName.contains("..") {
+            errorMessage.accept("Username may contain only one dot in a row")
+            return false
+        }
+
         // Rule 7
-        // • Each user name segment should start with a letter
-        let segments = userName.components(separatedBy: ".").filter({ !$0.isEmpty })
-        var segmentsStartLetter = true
-        var segmentLength = true
-        
-        if segments.count > 0 {
-            segmentsStartLetter = segments.filter({ Character($0.prefix(1).description).isNumber == true }).count == 0
-            segmentLength = segments.filter({ $0.count < 5}).count == 0
+        // Username may contain only one dot or one dash in a row
+        if userName.contains(".-") || userName.contains("-.") {
+            errorMessage.accept("Username may contain only one dot or one dash in a row")
+            return false
+        }
+
+        // Rule 8
+        // Username should end with a letter or a digit
+        if !(userName.last?.string.matches("^[a-z0-9].*$") ?? true) {
+            errorMessage.accept("Username should end with a letter or a digit")
+            return false
         }
         
-        var message: String?
-        if !isStartWithALetter {
-            message = "username should start with a letter"
-        }
-        
-        if message == nil && !isBetween5To32Characters {
-            message = "username must be between 5-32 characters"
-        }
-        
-        if message == nil && !containsOnlyAllowedCharacters {
-            message = "only non-uppercased letters, numbers and hyphen are allowed"
-        }
-        
-        if message == nil && !nonAlphanumericCharacterIsNotAtBeginOrEnd {
-            message = "the hyphen character cannot be at the beginning or at the end of the username"
-        }
-        
-        if message == nil && !twoNonAlphanumericCharacterNotSideBySide {
-            message = "the presence of two \"dots\" or two \"hyphens\" in a row is not valid"
-        }
-        
-        if message == nil && !segmentsStartLetter {
-            message = "each username segment should start with a letter"
-        }
-        
-        if message == nil && !segmentLength {
-            message = "each username segment should contains 5 or more letters"
-        }
-        
-        errorMessage.accept(message)
-        
-        return [
-            isStartWithALetter,
-            isBetween5To32Characters,
-            containsOnlyAllowedCharacters,
-            twoNonAlphanumericCharacterNotSideBySide,
-            nonAlphanumericCharacterIsNotAtBeginOrEnd,
-            segmentsStartLetter,
-            segmentLength
-        ]
-    }
-    
-    func isUserNameValid(_ userName: String) -> Bool {
-        checkUserName(userName).allSatisfy {$0}
+        return true
     }
 }
