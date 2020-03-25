86eea87fc29a8cbfc8de1db60e89cd2a6867b3a4
Chung Tran <bigearsenal@gmail.com>
2020-03-25 23:17:25 +0700

    add headerView

--- Commun/Controllers/ProfilePageVC/MyProfilePageVC/ViewControllers/ReferralUsersVC/ReferralUsersVC.swift
+++ Commun/Controllers/ProfilePageVC/MyProfilePageVC/ViewControllers/ReferralUsersVC/ReferralUsersVC.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 class ReferralUsersVC: SubsViewController<ResponseAPIContentGetProfile, SubscribersCell> {
+    lazy var headerView = ReferralHeaderView(tableView: tableView)
+    
     init() {
         let vm = ReferralUsersViewModel()
         super.init(viewModel: vm)
@@ -18,13 +20,38 @@ class ReferralUsersVC: SubsViewController<ResponseAPIContentGetProfile, Subscrib
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func setUp() {
+        super.setUp()
+        headerView.shareButton.addTarget(self, action: #selector(shareButtonDidTouch), for: .touchUpInside)
+        headerView.copyButton.addTarget(self, action: #selector(copyButtonDidTouch), for: .touchUpInside)
+        headerView.learnMoreButton.addTarget(self, action: #selector(infoButtonDidTouch), for: .touchUpInside)
+    }
+    
     override func handleListEmpty() {
         let title = "no referral users"
         let description = "no referral users found"
         tableView.addEmptyPlaceholderFooterView(title: title.localized().uppercaseFirst, description: description.localized().uppercaseFirst)
     }
     
+    override func configureCell(with item: ResponseAPIContentGetProfile, indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.configureCell(with: item, indexPath: indexPath) as! SubscribersCell
+        cell.followButton.isHidden = true
+        return cell
+    }
+    
     override func modelSelected(_ user: ResponseAPIContentGetProfile) {
         // do nothing
+    }
+    
+    @objc func shareButtonDidTouch() {
+        
+    }
+    
+    @objc func copyButtonDidTouch() {
+        
+    }
+    
+    @objc func infoButtonDidTouch() {
+        
     }
 }
