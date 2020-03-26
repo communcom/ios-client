50686504d23e4bad18ee062dffd68021851850d5
Chung Tran <bigearsenal@gmail.com>
2020-03-26 15:41:07 +0700

    add method cropImageView into ScrollView

--- Commun/Extensions/UIScrollView.swift
+++ Commun/Extensions/UIScrollView.swift
@@ -28,6 +28,48 @@ extension UIScrollView {
          let desiredOffset = CGPoint(x: 0, y: -contentInset.top)
          setContentOffset(desiredOffset, animated: true)
     }
+    
+    func cropImageView(
+        _ imageView: UIImageView,
+        maxSize: CGFloat = 1280,
+        disableHorizontal: Bool = false
+    ) -> UIImage? {
+        guard let originalImage = imageView.image else {
+            return nil
+        }
+        
+        let displayWidth = bounds.width
+        let displayHeight = bounds.height
+        
+        var proportion = originalImage.size.width < originalImage.size.height ? originalImage.size.width / displayWidth : originalImage.size.height / displayHeight
+        
+        if disableHorizontal {
+            proportion = originalImage.size.width / displayWidth
+        }
+        
+        proportion /= zoomScale
+        
+        let offsetX = contentOffset.x
+        let offsetY = contentOffset.y
+        
+        var finalRect = CGRect(
+            x: offsetX,
+            y: offsetY,
+            width: displayWidth,
+            height: bounds.height
+        )
+        
+        finalRect.origin.x *= proportion
+        finalRect.origin.y *= proportion
+        finalRect.size.width *= proportion
+        finalRect.size.height *= proportion
+        
+        // crop
+        let imageRef: CGImage = (originalImage.cgImage?.cropping(to: finalRect))!
+        let image: UIImage = UIImage(cgImage: imageRef, scale: originalImage.scale, orientation: originalImage.imageOrientation)
+        
+        return image.resize(maxSize)
+    }
 }
 
 extension Reactive where Base: UIScrollView {
