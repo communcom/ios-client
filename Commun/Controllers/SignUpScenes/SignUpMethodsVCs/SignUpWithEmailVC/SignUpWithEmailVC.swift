6a75cad72e2cb9d18302437004cc5076cc296e13
Chung Tran <bigearsenal@gmail.com>
2020-03-30 10:58:24 +0700

    Email validation

--- Commun/Controllers/SignUpScenes/SignUpMethodsVCs/SignUpWithEmailVC/SignUpWithEmailVC.swift
+++ Commun/Controllers/SignUpScenes/SignUpMethodsVCs/SignUpWithEmailVC/SignUpWithEmailVC.swift
@@ -9,17 +9,48 @@
 import Foundation
 
 class SignUpWithEmailVC: BaseSignUpMethodVC {
+    // MARK: - Properties
+    let viewModel = SignUpWithEmailViewModel()
+    
     // MARK: - Subviews
     lazy var textField = UITextField.signUpTextField(width: 290, placeholder: "your email address")
     
     // MARK: - Methods
+    override func setUp() {
+        super.setUp()
+        
+        // TODO: - Add analystic manager
+    }
+    
     override func setUpInputViews() {
         scrollView.contentView.addSubview(textField)
         textField.autoPinEdge(toSuperviewEdge: .top, withInset: UIScreen.main.isSmall ? 16 : 47)
         textField.autoAlignAxis(toSuperviewAxis: .vertical)
+        
+        scrollView.contentView.addSubview(errorLabel)
+        errorLabel.autoPinEdge(.top, to: .bottom, of: textField, withOffset: 10)
+        errorLabel.autoPinEdge(toSuperviewEdge: .leading, withInset: 32)
+        errorLabel.autoPinEdge(toSuperviewEdge: .trailing, withInset: 32)
     }
     
     override func pinBottomOfInputViews() {
-        termOfUseLabel.autoPinEdge(.top, to: .bottom, of: textField, withOffset: 30)
+        termOfUseLabel.autoPinEdge(.top, to: .bottom, of: errorLabel, withOffset: 30)
+    }
+    
+    override func bind() {
+        super.bind()
+        
+        let emailValidationObservable = textField.rx.text.orEmpty
+            .map {self.viewModel.isEmailValid($0)}
+        
+        emailValidationObservable
+            .filter {_ in self.textField.text!.isEmpty == false}
+            .map {$0 ? nil : "invalid email address".localized().uppercaseFirst}
+            .bind(to: errorLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        emailValidationObservable
+            .bind(to: nextButton.rx.isDisabled)
+            .disposed(by: disposeBag)
     }
 }
