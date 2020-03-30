d3731802567375b106a75533d0b0323f1a1ae0c0
Chung Tran <bigearsenal@gmail.com>
2020-03-30 15:33:48 +0700

    fix colon warning

--- Commun/Controllers/SignUpScenes/SignUpMethodsVCs/SignUpWithEmailVC/SignUpWithEmailViewModel.swift
+++ Commun/Controllers/SignUpScenes/SignUpMethodsVCs/SignUpWithEmailVC/SignUpWithEmailViewModel.swift
@@ -12,7 +12,7 @@ class SignUpWithEmailViewModel {
     func isEmailValid(_ email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
-        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: email)
     }
 }
