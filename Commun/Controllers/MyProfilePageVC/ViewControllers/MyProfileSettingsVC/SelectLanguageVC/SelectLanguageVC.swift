0c0758c2aca5a002ab8c905b7654c5bf884ee844
Chung Tran <bigearsenal@gmail.com>
2020-04-20 11:54:45 +0700

    show languages

--- Commun/Controllers/MyProfilePageVC/ViewControllers/MyProfileSettingsVC/SelectLanguageVC/SelectLanguageVC.swift
+++ Commun/Controllers/MyProfilePageVC/ViewControllers/MyProfileSettingsVC/SelectLanguageVC/SelectLanguageVC.swift
@@ -7,10 +7,29 @@
 //
 
 import Foundation
+import RxDataSources
+import RxCocoa
 
 class SelectLanguageVC: BaseViewController {
+    // MARK: - Nested type
+    struct Language: IdentifiableType, Equatable {
+        let code: String
+        let name: String
+        let imageName: String
+        var isSelected: Bool = false
+        var identity: String {code}
+    }
+    
+    // MARK: - Properties
+    let supportedLanguages: [Language] = [
+        Language(code: "en-EN", name: "english", imageName: "american-flag"),
+        Language(code: "ru-RU", name: "russian", imageName: "russian-flag")
+    ]
+    
     // MARK: - Subviews
     let closeButton = UIButton.close()
+    var tableView = UITableView(forAutoLayout: ())
+    lazy var languages = BehaviorRelay<[Language]>(value: supportedLanguages)
     
     // MARK: - Methods
     override func setUp() {
@@ -20,10 +39,37 @@ class SelectLanguageVC: BaseViewController {
         setRightNavBarButton(with: closeButton)
         closeButton.addTarget(self, action: #selector(back), for: .touchUpInside)
         
+        view.addSubview(tableView)
+        tableView.autoPinEdgesToSuperviewSafeArea(with: UIEdgeInsets(top: 20, left: 10, bottom: 0, right: 10))
+        tableView.register(LanguageCell.self, forCellReuseIdentifier: "LanguageCell")
+        tableView.separatorStyle = .none
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = .clear
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
+    override func bind() {
+        super.bind()
+        let dataSource = MyRxTableViewSectionedAnimatedDataSource<AnimatableSectionModel<String, Language>>(
+            configureCell: { _, _, indexPath, item in
+                let cell = self.tableView.dequeueReusableCell(withIdentifier: "LanguageCell", for: indexPath) as! LanguageCell
+                cell.setUp(with: item)
+                
+                cell.roundedCorner = []
+                
+                if indexPath.row == 0 {
+                    cell.roundedCorner.insert([.topLeft, .topRight])
+                }
+                
+                if indexPath.row == self.languages.value.count - 1 {
+                    cell.separator.isHidden = true
+                    cell.roundedCorner.insert([.bottomLeft, .bottomRight])
+                }
+                return cell
+            }
+        )
+        
+        languages.map {[AnimatableSectionModel<String, Language>](arrayLiteral: AnimatableSectionModel<String, Language>(model: "", items: $0))}
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 }
