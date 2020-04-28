6e44ce994edec00ab9aa61f5b670af61c360f9e5
Chung Tran <bigearsenal@gmail.com>
2020-04-28 15:45:22 +0700

    fix donationView

--- Commun/Vendor/UIViewControllers/PostsViewController/Cells/PostCell/DonationView.swift
+++ Commun/Vendor/UIViewControllers/PostsViewController/Cells/PostCell/DonationView.swift
@@ -15,8 +15,8 @@ class DonationView: CMMessageView {
     // MARK: - Subviews
     lazy var leadingLabel = UILabel.with(numberOfLines: 2)
     lazy var buttonStackView = UIStackView(axis: .horizontal, spacing: 5, alignment: .fill, distribution: .fill)
-    lazy var amountButtons: [UIButton] = amounts.map {UIButton(width: 60, label: "+\($0)", labelFont: .systemFont(ofSize: 12), backgroundColor: UIColor.white.withAlphaComponent(0.1), textColor: .white, cornerRadius: 17)}
-    lazy var otherButton = UIButton(width: 60, label: "other".localized().uppercaseFirst, labelFont: .systemFont(ofSize: 12), backgroundColor: UIColor.white.withAlphaComponent(0.1), textColor: .white, cornerRadius: 17)
+    lazy var amountButtons: [UIButton] = amounts.map {UIButton(width: 60, height: 34, label: "+\($0)", labelFont: .systemFont(ofSize: 12), backgroundColor: UIColor.white.withAlphaComponent(0.1), textColor: .white, cornerRadius: 17)}
+    lazy var otherButton = UIButton(width: 60, height: 34, label: "other".localized().uppercaseFirst, labelFont: .systemFont(ofSize: 12), backgroundColor: UIColor.white.withAlphaComponent(0.1), textColor: .white, cornerRadius: 17)
     
     // MARK: - Methods
     override func commonInit() {
