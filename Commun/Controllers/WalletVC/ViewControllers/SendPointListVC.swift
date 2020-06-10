678187a9bc38d69fba0d3fa771e5184e64f36a73
Chung Tran <bigearsenal@gmail.com>
2020-06-10 11:04:07 +0700

    add SearchBar on SendPointListVC

--- Commun/Controllers/WalletVC/ViewControllers/SendPointListVC.swift
+++ Commun/Controllers/WalletVC/ViewControllers/SendPointListVC.swift
@@ -8,13 +8,23 @@
 
 import Foundation
 
-class SendPointListVC: SubscriptionsVC {
+class SendPointListVC: SubscriptionsVC, SearchableViewControllerType {
     // MARK: - Properties
     var completion: ((ResponseAPIContentGetProfile) -> Void)?
+    var tableViewTopConstraint: NSLayoutConstraint?
+    
+    // MARK: - Subviews
+    let searchController = UISearchController.default()
+    lazy var searchContainerView = UIView(backgroundColor: .appWhiteColor)
+    var searchBar: UISearchBar {
+        get {searchController.searchBar}
+        set {}
+    }
     
     // MARK: - Initializers
     init() {
         super.init(title: "send points".localized().uppercaseFirst, type: .user)
+        showShadowWhenScrollUp = false
     }
     
     required init?(coder: NSCoder) {
@@ -22,6 +32,40 @@ class SendPointListVC: SubscriptionsVC {
     }
     
     // MARK: - Methods
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchBar.roundCorner()
+    }
+    
+    override func bind() {
+        super.bind()
+        bindSearchBar()
+        
+        searchBar.rx.textDidBeginEditing
+            .subscribe(onNext: { (_) in
+                self.showSearchBar(onNavigationBar: true)
+            })
+            .disposed(by: disposeBag)
+        
+        searchBar.rx.textDidEndEditing
+            .subscribe(onNext: { (_) in
+                self.showSearchBar(onNavigationBar: false)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    override func viewWillSetUpTableView() {
+        super.viewWillSetUpTableView()
+        layoutSearchBar()
+    }
+    
+    override func setUpTableView() {
+        super.setUpTableView()
+        tableView.removeConstraintToSuperView(withAttribute: .top)
+        
+        tableViewTopConstraint = tableView.autoPinEdge(.top, to: .bottom, of: searchBar)
+    }
+    
     override func configureCell(with subscription: ResponseAPIContentGetSubscriptionsItem, indexPath: IndexPath) -> UITableViewCell {
         let cell = super.configureCell(with: subscription, indexPath: indexPath) as! SubscriptionsUserCell
         cell.hideActionButton()
@@ -32,5 +76,56 @@ class SendPointListVC: SubscriptionsVC {
         guard let user = item.userValue else {return}
         self.completion?(user)
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Search manager
+    private func showSearchBar(onNavigationBar: Bool) {
+        if onNavigationBar {
+            navigationItem.titleView = searchBar
+            navigationItem.rightBarButtonItem = nil
+            
+            tableViewTopConstraint?.isActive = false
+            
+            searchContainerView.removeFromSuperview()
+            tableViewTopConstraint = tableView.autoPinEdge(toSuperviewSafeArea: .top)
+            
+            resetNavigationBar()
+        } else {
+            navigationItem.titleView = nil
+            setRightNavBarButton(with: self.closeButton)
+            
+            tableViewTopConstraint?.isActive = false
+            layoutSearchBar()
+            tableViewTopConstraint = tableView.autoPinEdge(.top, to: .bottom, of: searchContainerView)
+            
+            resetNavigationBar()
+        }
+    }
+    
+    private func resetNavigationBar() {
+        let img = UIImage()
+        navigationController?.navigationBar.setBackgroundImage(img, for: .default)
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.navigationBar.barTintColor = .appWhiteColor
+        navigationController?.navigationBar.subviews.first?.backgroundColor = .appWhiteColor
+    }
+    
+    func layoutSearchBar() {
+        view.addSubview(searchContainerView)
+        searchContainerView.autoPinEdgesToSuperviewSafeArea(with: .zero, excludingEdge: .bottom)
+        searchContainerView.addSubview(searchBar)
+        
+        searchBar.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: -10, left: 0, bottom: 0, right: 0))
+        DispatchQueue.main.async {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func searchBarIsSearchingWithQuery(_ query: String) {
+        print(query)
+    }
+    
+    func searchBarDidCancelSearching() {
+        print("cancel search")
     }
 }
