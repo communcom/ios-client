889a533426f5d36e7c6e60584d2ed93c033d54f0
Chung Tran <bigearsenal@gmail.com>
2020-06-24 10:30:29 +0700

    show/hide password

--- Commun/Controllers/MyProfilePageVC/ViewControllers/MyProfileSettingsVC/PasswordsVC/PasswordsVC.swift
+++ Commun/Controllers/MyProfilePageVC/ViewControllers/MyProfileSettingsVC/PasswordsVC/PasswordsVC.swift
@@ -7,13 +7,19 @@
 //
 
 import Foundation
+import RxCocoa
 
 class PasswordsVC: BaseViewController {
     // MARK: - Properties
+    var authenticated = false
+    let showPasswordSubject = BehaviorRelay<Bool>(value: false)
     
     // MARK: - Subviews
     lazy var scrollView = ContentHuggingScrollView(scrollableAxis: .vertical)
     lazy var stackView = UIStackView(axis: .vertical, spacing: 10, alignment: .fill, distribution: .fill)
+    lazy var passwordField = secretFieldWithTitle("password".localized().uppercaseFirst)
+    lazy var ownerField = secretFieldWithTitle("owner".localized().uppercaseFirst)
+    lazy var activeField = secretFieldWithTitle("active".localized().uppercaseFirst)
     
     // MARK: - Methods
     override func setUp() {
@@ -35,10 +41,25 @@ class PasswordsVC: BaseViewController {
         
         // password
         stackView.addArrangedSubviews([
-            secretFieldWithTitle("password".localized().uppercaseFirst),
-            secretFieldWithTitle("owner".localized().uppercaseFirst),
-            secretFieldWithTitle("active".localized().uppercaseFirst)
+            passwordField,
+            ownerField,
+            activeField
         ])
+    }
+    
+    override func bind() {
+        super.bind()
+        showPasswordSubject
+            .subscribe(onNext: { (show) in
+                if show {
+                    (self.passwordField.viewWithTag(1) as! UILabel).text = "test"
+                } else {
+                    (self.passwordField.viewWithTag(1) as! UILabel).text = "••••••••••••••••••••••••••••"
+                    (self.ownerField.viewWithTag(1) as! UILabel).text = "••••••••••••••••••••••••••••"
+                    (self.activeField.viewWithTag(1) as! UILabel).text = "••••••••••••••••••••••••••••"
+                }
+            })
+            .disposed(by: disposeBag)
     }
     
     private func secretFieldWithTitle(_ title: String) -> UIView {
@@ -50,9 +71,11 @@ class PasswordsVC: BaseViewController {
         
         let passwordField = UIView(backgroundColor: .clear)
         
-        let passwordLabel = UILabel.with(text: "••••••••••••••••••••••••••••", textSize: 17, weight: .medium)
+        let passwordLabel = UILabel.with(textSize: 17, weight: .medium)
+        passwordLabel.tag = 1
         let showPasswordButton = UIButton(width: 24, height: 16)
         showPasswordButton.setImage(UIImage(named: "show-password"), for: .normal)
+        showPasswordButton.addTarget(self, action: #selector(toggleShowHidePassword), for: .touchUpInside)
         
         passwordField.addSubview(passwordLabel)
         passwordLabel.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .trailing)
@@ -67,5 +90,20 @@ class PasswordsVC: BaseViewController {
         stackView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 16))
         
         return view
+    }
+    
+    @objc private func toggleShowHidePassword() {
+        if showPasswordSubject.value {
+            showPasswordSubject.accept(false)
+            return
+        }
+        
+        if authenticated {
+            showPasswordSubject.accept(true)
+            return
+        }
+        
+        // TODO: - Authentication
+        showPasswordSubject.accept(true)
     }
 }
