8d62a23ce8a6422be2fc876125151330b3cda13e
Chung Tran <bigearsenal@gmail.com>
2020-06-24 15:00:03 +0700

    fix sender, buyer

--- Commun/Vendor/UIViewControllers/TransferHistoryVC/TransferHistoryVC.swift
+++ Commun/Vendor/UIViewControllers/TransferHistoryVC/TransferHistoryVC.swift
@@ -93,28 +93,37 @@ class TransferHistoryVC: ListViewController<ResponseAPIWalletGetTransferHistoryI
         var amount: CGFloat = 0.0
         var symbol: Symbol = Symbol(sell: selectedItem.symbol, buy: selectedItem.symbol)
         
+        var isReceiver = false
+        var person = selectedItem.receiver
+        if person.userId == Config.currentUser?.id {
+            person = selectedItem.sender
+            isReceiver = true
+        }
+        friend = Friend(id: person.userId, name: person.username ?? Config.defaultSymbol, avatarURL: person.avatarUrl)
+        amount = (isReceiver ? 1 : -1) * CGFloat(selectedItem.quantityValue)
+        
+        if friend?.id == Config.currentUser?.id {
+            friend = nil
+        }
+        
         switch selectedItem.meta.actionType {
-        case "transfer", "referralRegisterBonus", "referralPurchaseBonus":
-            let receiver = selectedItem.receiver
-            friend = Friend(id: receiver.userId, name: receiver.username ?? Config.defaultSymbol, avatarURL: receiver.avatarUrl)
-            let isCurrent = Config.currentUser?.id == receiver.userId
-            amount = (isCurrent ? 1 : -1) * CGFloat(selectedItem.quantityValue)
-            
         case "convert":
             amount = CGFloat(selectedItem.meta.exchangeAmount ?? 0.0)
             
             // Sell `MEME` -> buy `CMN`
             if selectedItem.symbol != Config.defaultSymbol {
                 symbol.buy = Config.defaultSymbol
+                symbol.sell = selectedItem.symbol
             }
             
             // Sell `CMN` -> buy `MEME`
             else {
                 symbol.sell = Config.defaultSymbol
+                symbol.buy = selectedItem.symbol
             }
             
         default:
-            amount = CGFloat(selectedItem.quantityValue * (selectedItem.meta.actionType == "transfer" ? -1 : 1))
+            break
         }
         
         let transaction = Transaction(buyBalance: nil,
