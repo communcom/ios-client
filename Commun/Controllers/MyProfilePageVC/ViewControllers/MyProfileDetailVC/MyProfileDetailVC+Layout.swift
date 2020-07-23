c4df4440960329c9b828fffb8e513a8551818ccc
Chung Tran <bigearsenal@gmail.com>
2020-07-23 15:20:06 +0700

    refactor

--- Commun/Controllers/MyProfilePageVC/ViewControllers/MyProfileDetailVC/MyProfileDetailVC+Layout.swift
+++ Commun/Controllers/MyProfilePageVC/ViewControllers/MyProfileDetailVC/MyProfileDetailVC+Layout.swift
@@ -1,66 +1,14 @@
 //
-//  MyProfileEditVC.swift
+//  MyProfileDetailVC+Layout.swift
 //  Commun
 //
-//  Created by Chung Tran on 3/26/20.
+//  Created by Chung Tran on 7/23/20.
 //  Copyright © 2020 Commun Limited. All rights reserved.
 //
 
 import Foundation
 
-class MyProfileDetailVC: BaseVerticalStackVC {
-    // MARK: - Properties
-    var profile: ResponseAPIContentGetProfile?
-    
-    // MARK: - Subviews
-    var spacer: UIView { UIView(height: 2, backgroundColor: .appLightGrayColor)}
-    
-    // MARK: - Sections
-    lazy var generalInfoView = UIView(backgroundColor: .appWhiteColor, cornerRadius: 10)
-    lazy var contactsView = UIView(backgroundColor: .appWhiteColor, cornerRadius: 10)
-    lazy var linksView = UIView(backgroundColor: .appWhiteColor, cornerRadius: 10)
-    
-    // MARK: - Methods
-    override func setUp() {
-        super.setUp()
-        title = "my profile".localized().uppercaseFirst
-        
-        reloadData()
-    }
-    
-    override func bind() {
-        super.bind()
-        UserDefaults.standard.rx.observe(Data.self, Config.currentUserGetProfileKey)
-            .filter {$0 != nil}
-            .map {$0!}
-            .map {try? JSONDecoder().decode(ResponseAPIContentGetProfile.self, from: $0)}
-            .subscribe(onNext: { profile in
-                self.profile = profile
-                self.reloadData()
-            })
-            .disposed(by: disposeBag)
-    }
-    
-    override func setUpArrangedSubviews() {
-        stackView.addArrangedSubviews([
-            generalInfoView,
-            contactsView,
-            linksView
-        ])
-    }
-    
-    override func viewDidSetUpStackView() {
-        super.viewDidSetUpStackView()
-        stackView.spacing = 20
-    }
-    
-    // MARK: - Data handler
-    func reloadData() {
-        updateGeneralInfo()
-        updateContacts()
-        updateLinks()
-    }
-    
+extension MyProfileDetailVC {
     func updateGeneralInfo() {
         generalInfoView.removeSubviews()
         let stackView = UIStackView(axis: .vertical, spacing: 0, alignment: .center, distribution: .fill)
