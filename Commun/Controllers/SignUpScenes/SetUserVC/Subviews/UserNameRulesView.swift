2ca2329094c7dd7b1a24b4431c6957c4368272d0
Chung Tran <bigearsenal@gmail.com>
2020-07-24 11:20:11 +0700

    add localization for UserNameRulesView

--- Commun/Controllers/SignUpScenes/SetUserVC/Subviews/UserNameRulesView.swift
+++ Commun/Controllers/SignUpScenes/SetUserVC/Subviews/UserNameRulesView.swift
@@ -26,12 +26,12 @@ class UserNameRulesView: MyCardView {
         titleLabel.autoAlignAxis(.horizontal, toSameAxisOf: closeButton)
         
         let rulesText = """
-        •  The number of characters must not exceed 32
-        •  Uppercase letters in the username are not allowed
-        •  Valid characters: letters, numbers, hyphen
-        •  The hyphen character cannot be at the beginning or at the end of the username
-        •  The user name may contain a "dot" character
-        •  The presence of two "dot" characters in a row is not valid
+        •  \("the number of characters must not exceed 32".localized().uppercaseFirst)
+        •  \("uppercase letters in the username are not allowed".localized().uppercaseFirst)
+        •  \("valid characters: letters, numbers, hyphen".localized().uppercaseFirst)
+        •  \("the hyphen character cannot be at the beginning or at the end of the username".localized().uppercaseFirst)
+        •  \("the user name may contain a \"dot\" character".localized().uppercaseFirst)
+        •  \("the presence of two \"dot\" characters in a row is not valid".localized().uppercaseFirst)
         """
         
         let attributedString = NSMutableAttributedString(string: viewParameters == .user ? rulesText : viewParameters.note)
