3a67e605f71cd3ece8b2cd661278614fce685370
Chung Tran <bigearsenal@gmail.com>
2020-09-03 10:18:57 +0700

    show Title in PostCell, PostPage

--- Commun/Controllers/PostPageVC/Subviews/Header/PostHeaderView/PostHeaderView.swift
+++ Commun/Controllers/PostPageVC/Subviews/Header/PostHeaderView/PostHeaderView.swift
@@ -29,6 +29,8 @@ class PostHeaderView: MyTableHeaderView {
     weak var delegate: PostHeaderViewDelegate?
 
     // MARK: - Subviews
+    lazy var stackView = UIStackView(axis: .vertical, spacing: 0, alignment: .center, distribution: .fill)
+    
     lazy var titleLabel = UILabel.with(text: "", textSize: 21, weight: .bold, numberOfLines: 0)
     
     lazy var contentTextView = PostHeaderTextView(forExpandable: ())
@@ -41,40 +43,36 @@ class PostHeaderView: MyTableHeaderView {
     
 //    lazy var sortButton = RightAlignedIconButton(imageName: "small-down-arrow", label: "interesting first".localized().uppercaseFirst, labelFont: .boldSystemFont(ofSize: 13), textColor: .appMainColor, contentInsets: UIEdgeInsets(horizontal: 8, vertical: 0))
     
-    // MARK: - Constraints
-    var contentTextViewTopConstraint: NSLayoutConstraint?
+    // MARK: - Methods
     
     override func commonInit() {
         super.commonInit()
+        addSubview(stackView)
+        stackView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 0, left: 0, bottom: 16, right: 0))
         
-        addSubview(contentTextView)
-        contentTextViewTopConstraint = contentTextView.autoPinEdge(toSuperviewEdge: .top)
-        contentTextView.autoPinEdge(toSuperviewEdge: .leading)
-        contentTextView.autoPinEdge(toSuperviewEdge: .trailing)
+        let commentsLabel = UILabel.with(text: "comments".localized().uppercaseFirst, textSize: 21, weight: .bold)
+        stackView.addArrangedSubviews([
+            titleLabel,
+            contentTextView,
+            postStatsView,
+            commentsLabel
+        ])
+        
+        stackView.setCustomSpacing(10, after: contentTextView)
+        stackView.setCustomSpacing(16, after: postStatsView)
+        
+        titleLabel.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -32).isActive = true
+        contentTextView.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+        postStatsView.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -32).isActive = true
+        commentsLabel.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -32).isActive = true
+        
         contentTextView.delegate = self
-        
-        addSubview(postStatsView)
-        postStatsView.autoPinEdge(.top, to: .bottom, of: contentTextView, withOffset: 10)
-        postStatsView.autoPinEdge(toSuperviewEdge: .leading, withInset: .adaptive(width: 16))
-        postStatsView.autoPinEdge(toSuperviewEdge: .trailing, withInset: .adaptive(width: 16))
         
         postStatsView.voteContainerView.upVoteButton.addTarget(self, action: #selector(upVoteButtonDidTouch(_:)), for: .touchUpInside)
         postStatsView.voteContainerView.downVoteButton.addTarget(self, action: #selector(downVoteButtonDidTouch(_:)), for: .touchUpInside)
         postStatsView.shareButton.addTarget(self, action: #selector(shareButtonDidTouch(_:)), for: .touchUpInside)
         
         postStatsView.commentsCountButton.addTarget(self, action: #selector(commentsCountButtonDidTouch), for: .touchUpInside)
-        
-        let commentsLabel = UILabel.with(text: "comments".localized().uppercaseFirst, textSize: 21, weight: .bold)
-        addSubview(commentsLabel)
-        commentsLabel.autoPinEdge(.top, to: .bottom, of: postStatsView, withOffset: 20)
-        commentsLabel.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
-        
-//        addSubview(sortButton)
-//        sortButton.autoPinEdge(toSuperviewEdge: .trailing, withInset: 16)
-//        sortButton.autoAlignAxis(.horizontal, toSameAxisOf: commentsLabel)
-        
-        // Pin bottom
-        commentsLabel.autoPinEdge(toSuperviewEdge: .bottom, withInset: 16)
         
         // donation
         addSubview(donationUsersView)
@@ -102,24 +100,11 @@ class PostHeaderView: MyTableHeaderView {
     }
     
     func setUp(with post: ResponseAPIContentGetPost) {
-        titleLabel.removeFromSuperview()
-        contentTextViewTopConstraint?.isActive = false
-        
-        if post.document?.attributes?.type == "article" {
-            // Show title
-            titleLabel.text = post.document?.attributes?.title
-            
-            addSubview(titleLabel)
-            titleLabel.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(inset: 16), excludingEdge: .bottom)
-            
-            contentTextViewTopConstraint = contentTextView.autoPinEdge(.top, to: .bottom, of: titleLabel)
-            contentTextViewTopConstraint?.isActive = true
-
+        // Show title
+        if let title = post.title, !title.trimmed.isEmpty {
+            titleLabel.text = title
         } else {
-            titleLabel.text = nil
-            
-            contentTextViewTopConstraint = contentTextView.autoPinEdge(toSuperviewEdge: .top)
-            contentTextViewTopConstraint?.isActive = true
+            titleLabel.isHidden = true
         }
         
         postStatsView.setUp(with: post)
@@ -156,7 +141,7 @@ class PostHeaderView: MyTableHeaderView {
             donationView.isHidden = false
         }
         
-        layoutSubviews()
+        setNeedsLayout()
     }
     
     // MARK: - Actions
