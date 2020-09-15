735d1e40cd99fb2c5ea3d3b7824e00262d43dcdb
Chung Tran <bigearsenal@gmail.com>
2020-09-15 14:23:59 +0700

    Refactor CommunityRuleCell

--- Commun/Controllers/CommunityPageVC/Subviews/CommunityRuleCell.swift
+++ Commun/Controllers/CommunityPageVC/Subviews/CommunityRuleCell.swift
@@ -12,61 +12,53 @@ class CommunityRuleCell: CommunityPageCell {
     // MARK: - Properties
     var rowIndex: Int?
     var rule: ResponseAPIContentGetCommunityRule?
-    var bottomConstraint: NSLayoutConstraint?
     
     // MARK: - Subviews
     lazy var containerView = UIView(backgroundColor: .appWhiteColor, cornerRadius: 10)
-    lazy var titleLabel = UILabel.with(text: "2. Content should be Safe for Work", textSize: 15.0, weight: .bold, numberOfLines: 0)
-    lazy var contentLabel = UILabel.with(text: "All content (title, articles, video, image, website, etc.) must be SFW: Safe For Work. Content that is NSFW: Not Safe For Work, is banned. This rule applies to all posts and comments.", textSize: 15.0, numberOfLines: 0)
-    lazy var expandButton = UIButton.circleGray(imageName: "rule_expand")
+    lazy var stackView = UIStackView(axis: .vertical, spacing: 14, alignment: .fill, distribution: .fill)
+    lazy var titleLabel: UILabel = {
+        let label = UILabel.with(text: "2. Content should be Safe for Work", textSize: 15.0, weight: .bold, numberOfLines: 0)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        label.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        return label
+    }()
+    lazy var contentLabel: UILabel = UILabel.with(text: "All content (title, articles, video, image, website, etc.) must be SFW: Safe For Work. Content that is NSFW: Not Safe For Work, is banned. This rule applies to all posts and comments.", textSize: 15.0, numberOfLines: 0)
+    lazy var expandButton: UIButton = {
+        let expandButton = UIButton.circleGray(imageName: "rule_expand")
+        expandButton.addTarget(self, action: #selector(expandButtonDidTouch(_:)), for: .touchUpInside)
+        return expandButton
+    }()
     
     override func setUpViews() {
         super.setUpViews()
         // background color
-        contentView.backgroundColor = .appLightGrayColor
+        contentView.backgroundColor = .clear
         
         contentView.addSubview(containerView)
         containerView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 0.0, left: 10.0, bottom: 10.0, right: 10.0))
         
-        containerView.addSubview(titleLabel)
-        titleLabel.autoPinTopAndLeadingToSuperView(inset: 20, xInset: 16)
+        containerView.addSubview(stackView)
+        stackView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16))
         
-        containerView.addSubview(expandButton)
-        expandButton.autoPinTopAndTrailingToSuperView(inset: 20.0, xInset: 16)
-        expandButton.autoPinEdge(.leading, to: .trailing, of: titleLabel, withOffset: 8.0)
-        expandButton.addTarget(self, action: #selector(expandButtonDidTouch(_:)), for: .touchUpInside)
+        let titleStackView = UIStackView(axis: .horizontal, spacing: 8, alignment: .top, distribution: .fill)
+        titleStackView.addArrangedSubview(titleLabel)
+        titleStackView.addArrangedSubview(expandButton)
+        
+        stackView.addArrangedSubviews([
+            titleStackView,
+            contentLabel
+        ])
+        
+        contentLabel.isHidden = true
     }
     
     func setUp(with newRule: ResponseAPIContentGetCommunityRule?) {
         rule = newRule
         titleLabel.text = "\((rowIndex ?? 0) + 1). " + (rule?.title ?? "")
         expandButton.isHidden = (newRule?.text == nil) || (newRule?.text?.isEmpty ?? false)
-        setExpanded()
-    }
-    
-    func setExpanded() {
-        guard let rule = rule else {return}
-        
-        bottomConstraint?.isActive = false
-        contentLabel.removeFromSuperview()
-        
-        if rule.isExpanded == true {
-            contentLabel.text = rule.text
-            expandButton.setImage(UIImage(named: "rule_collapse"), for: .normal)
-            
-            // add contentLabel
-            containerView.addSubview(contentLabel)
-            contentLabel.autoPinEdge(.top, to: .bottom, of: titleLabel, withOffset: 14)
-            contentLabel.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
-            contentLabel.autoPinEdge(toSuperviewEdge: .trailing, withInset: 16)
-            bottomConstraint = contentLabel.autoPinEdge(toSuperviewEdge: .bottom, withInset: 20)
-        } else {
-            contentLabel.text = nil
-            expandButton.setImage(UIImage(named: "rule_expand"), for: .normal)
-            
-            // pin bottom
-            bottomConstraint = titleLabel.autoPinEdge(toSuperviewEdge: .bottom, withInset: 20)
-        }
+        let isExpanded = (rule?.isExpanded ?? false)
+        contentLabel.isHidden = !isExpanded
+        expandButton.transform = isExpanded ? .init(rotationAngle: -.pi) : .identity
     }
     
     @objc func expandButtonDidTouch(_ sender: UIButton) {
