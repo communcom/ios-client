4aecced63f8633f198f46ae3fa9e816f11d3de47
Chung Tran <bigearsenal@gmail.com>
2020-09-17 12:03:24 +0700

    change topics

--- Commun/Controllers/CommunityPageVC/CommunityManager/EditCommunity/EditTopicsVC/TopicsVC.swift
+++ Commun/Controllers/CommunityPageVC/CommunityManager/EditCommunity/EditTopicsVC/TopicsVC.swift
@@ -18,6 +18,9 @@ class TopicsVC: CMTableViewController<String, TopicCell>, UITableViewDelegate {
     let communityCode: String
     let communityIssuer: String
     
+    
+    lazy var saveButton = UIBarButtonItem(title: "save".localized().uppercaseFirst, style: .done, target: self, action: #selector(saveButtonDidTouch))
+    
     // MARK: - Initializers
     init(communityCode: String, communityIssuer: String, topics: [String]) {
         self.communityCode = communityCode
@@ -35,6 +38,11 @@ class TopicsVC: CMTableViewController<String, TopicCell>, UITableViewDelegate {
     override func setUp() {
         super.setUp()
         title = "topics".localized().uppercaseFirst
+        
+        setLeftBarButton(imageName: "icon-back-bar-button-black-default", tintColor: .appBlackColor, action: #selector(askForSavingAndGoBack))
+        
+        saveButton.tintColor = .appBlackColor
+        navigationItem.rightBarButtonItem = saveButton
         
         // footerView
         let footerView: UIView = {
@@ -70,6 +78,11 @@ class TopicsVC: CMTableViewController<String, TopicCell>, UITableViewDelegate {
 //        tableView.rx.setDelegate(self)
 //            .disposed(by: disposeBag)
         
+        itemsRelay.map {_ in self.dataHasChanged()}
+            .asDriver(onErrorJustReturn: false)
+            .drive(saveButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
         tableView.rx.itemDeleted
             .subscribe(onNext: { (indexPath) in
                 var items = self.itemsRelay.value
@@ -104,6 +117,10 @@ class TopicsVC: CMTableViewController<String, TopicCell>, UITableViewDelegate {
             .disposed(by: disposeBag)
     }
     
+    private func dataHasChanged() -> Bool {
+        itemsRelay.value != originalItems
+    }
+    
     override func configureCell(item: String, indexPath: IndexPath) -> UITableViewCell {
         let cell = super.configureCell(item: item, indexPath: indexPath) as! TopicCell
         cell.label.text = item
@@ -135,6 +152,40 @@ class TopicsVC: CMTableViewController<String, TopicCell>, UITableViewDelegate {
 
         // 4. Present the alert.
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func saveButtonDidTouch() {
+        self.showIndetermineHudWithMessage("creating proposal".localized().uppercaseFirst)
+        BlockchainManager.instance.editCommunnity(
+            communityCode: self.communityCode,
+            commnityIssuer: self.communityIssuer,
+            subject: itemsRelay.value.convertToJSON()
+        )
+            .flatMapCompletable {RestAPIManager.instance.waitForTransactionWith(id: $0)}
+            .subscribe(onCompleted: {
+                self.hideHud()
+                self.showAlert(title: "proposal created".localized().uppercaseFirst, message: "proposal for topics changing has been created".localized().uppercaseFirst) { (_) in
+                    self.back()
+                }
+            }) { (error) in
+                self.hideHud()
+                self.showError(error)
+            }
+            .disposed(by: self.disposeBag)
+    }
+    
+    @objc func askForSavingAndGoBack() {
+        if dataHasChanged() {
+            showAlert(title: "save".localized().uppercaseFirst, message: "do you want to save the changes you've made?".localized().uppercaseFirst, buttonTitles: ["yes".localized().uppercaseFirst, "no".localized().uppercaseFirst], highlightedButtonIndex: 0) { (index) in
+                if index == 0 {
+                    self.saveButtonDidTouch()
+                    return
+                }
+                self.back()
+            }
+        } else {
+            back()
+        }
     }
     
 //    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
