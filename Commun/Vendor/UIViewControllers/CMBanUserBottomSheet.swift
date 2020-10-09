9bc938007404de3c51b88eec886037dad7c74917
Chung Tran <bigearsenal@gmail.com>
2020-10-09 12:14:45 +0700

    some fixes

--- Commun/Vendor/UIViewControllers/CMBanUserBottomSheet.swift
+++ Commun/Vendor/UIViewControllers/CMBanUserBottomSheet.swift
@@ -129,9 +129,9 @@ class CMBanUserBottomSheet: CMBottomSheet {
             showAlert(title: "no reason".localized().uppercaseFirst, message: "you must choose at least 1 reason to ban this user".localized().uppercaseFirst)
             return
         }
-        if user.isBanProposalCreated == true {
+        if banningUser.isBanProposalCreated == true {
             self.showAlert(title: "proposal created".localized().uppercaseFirst, message: "you've already created proposal for banning this user".localized().uppercaseFirst)
-            return .just(())
+            return
         }
         showIndetermineHudWithMessage("creating proposal".localized().uppercaseFirst)
         let reasonString = "[" + reasons.0.inlineString(otherReason: reasons.1, shouldNormalize: true) + "]"
